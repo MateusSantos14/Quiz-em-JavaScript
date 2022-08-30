9998ff6280ba31f7c76c41428a38dba79d54100e
Mateus Santos <msantos9943@gmail.com>
2022-08-30 18:23:40 -0300

    Contagem de pontos implementda

--- script/script.js
+++ script/script.js
@@ -12,9 +12,11 @@ function acessar_api()
         var element = document.getElementById("enviar");
         element.addEventListener("click",() => {
             var valores = element.value.split(":");
-            console.log(valores);
+            console.log(questoes);
+            //console.log(valores);
             valores = proximo_passo(valores,questoes);
             element.value = valores;
+
         },false);
 
     }
@@ -27,28 +29,46 @@ function proximo_passo(valores,questoes)//Id_questao varia de 0 a 3
 {
     var id_questao = valores[0];
     var pontuacao = valores[1];
+    var opcao_marcada = 0; 
+    if(id_questao !=0)
+    {
+        //Encontra a opção marcada e se tiver opção marcada, calcula a pontuação.
+        var indice;
+        for(var x = 0;x<4;x++)
+        {
+            if(document.getElementById(`opcao${x+1}`).checked)
+            {
+                document.getElementById(`opcao${x+1}`).checked = false;
+                pontuacao = parseInt(pontuacao) + parseInt(questoes[id_questao-1]["options"][x]["value"]);
+                opcao_marcada = 1;
+            }
+        }
+        //Tratar situação em que não há opção marcada
+        if(opcao_marcada == 0)
+        {
+            modificarPeloId("Marque uma opção","aviso");
+            return `${parseInt(id_questao)}:${pontuacao}`;
+        }
+    }
     if(id_questao == 5)
     {
-        console.log("fim");
+        document.getElementById("alternativas").style.display = "none";
+        modificarPeloId(`Quiz encerrado Pontuação: ${pontuacao}`,"aviso");
         return "0:0";
     }
     else
     {
-        for(var x = 0;x<4;x++)
-        {
-            if(document.getElementById(`opcao${x+1}`).checked == true)
-            {
-                pontuacao = parseInt(pontuacao) + parseInt(questoes[id_questao]["options"][x]["value"]);
-            }
-        }
-        modificarPeloId(questoes[id_questao]["title"],"pergunta");
-        for(var x = 0;x<4;x++)
-        {
-            modificarPeloId(questoes[id_questao]["options"][x]["answer"],`Label_Opcao_${x+1}`)
-        }
-        return `${parseInt(id_questao)+1}:${pontuacao}`;
+        document.getElementById("alternativas").style.display = "block";
     }
     
+    //Muda os dados para a próxima questão
+    modificarPeloId(questoes[id_questao]["title"],"pergunta");
+    for(var x = 0;x<4;x++)
+    {
+        modificarPeloId(questoes[id_questao]["options"][x]["answer"],`Label_Opcao_${x+1}`)
+    }
+    modificarPeloId("","aviso");
+    return `${parseInt(id_questao)+1}:${pontuacao}`;
     
 }
 // Função para mudar o conteúdo de uma tag html pelo id
